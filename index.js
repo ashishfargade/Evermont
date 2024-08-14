ddb1a12b95efa58e0c07967e37d6d8c50737f27a
ashishfargade <fargadeashish@gmail.com>
2024-08-14 20:18:51 +0530

    explore section done

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     const navLinks = document.querySelector('#nav-links');
     navLinks.classList.toggle('active');
 }
+
+function toggleFeatures(id) {
+    const content = document.getElementById(id);
+    
+    if (content.style.display === "none" || content.style.display === "") {
+        content.style.display = "flex";
+    } else {
+        content.style.display = "none";
+    }
+}
